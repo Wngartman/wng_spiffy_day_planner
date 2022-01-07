7df34adba39f60c6d0bdc70c4bfbadfcc061639a
William Gartman <wngartman@gmail.com>
2022-01-07 16:19:52 -0600

    Added the times for each colomn

---
+++ Develop/script.js
@@ -0,0 +1,8 @@
+// I want to have a daily date on the page
+$("#currentDay").text(moment().format("MMM Do YY"));
+
+// I want the colors to change with the time
+
+// I want to save to local storage
+
+// Add an alert msg "I just saved to local storage"
